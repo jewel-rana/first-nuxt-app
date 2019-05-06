269d3d4dd67d1ec510007c0f673b4b16e4329ece
jewelranadev <jewelrana.dev@gmail.com>
2019-05-06 17:41:03 +0600

    Configure nuxt

---
+++ components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="nav">
+      <nuxt-link to="/">My Nuxt App</nuxt-link>
+      <nav>
+        <nuxt-link to="/">Events</nuxt-link>
+        <nuxt-link to="/create">Create event</nuxt-link>
+      </nav>
+    </div>
+  </div>
+</template>
